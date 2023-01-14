49191f9e7ed7f2b66f44a40898bfd6e1943ce043
Zachary Keyes <zachk@arizona.edu>
2023-01-14 13:23:10 -0700

    rework

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Berserk-CLI
+# berserk-CLI
 
 Berserk-CLI is a command-line implementation of LiChess' Berserk API. Development is on the backburners for now.
---
+++ src/berserk-CLI/main.py
@@ -0,0 +1,10 @@
+'''
+import berserk
+import chess
+
+session = berserk.TokenSession(API_TOKEN)
+client = berserk.Client(session=session)
+'''
+
+def add_two(number):
+    return number + 2
